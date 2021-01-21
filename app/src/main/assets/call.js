b4bdbadf2d624af73eb15010ea522462c7b869ec
HE-Kim <kimhe045@gmail.com>
2021-01-21 10:05:29 +0900

    2021.01.21 수정

--- app/src/main/assets/call.js
+++ app/src/main/assets/call.js
@@ -13,7 +13,7 @@ let peer //잘은 모르겠지만 peerjs.js파일에 존재하는 녀석..!
 // 웹의 콘솔에서 우리가 init("A")로 입력하면 peerjs서버에서 클라이언트 연결되었다고 할때 그것!
 function init(userId) {
     peer = new Peer(userId, {
-        host: '172.30.1.52',//'192.168.0.60',//'192.168.43.242',
+        host: '172.30.1.52',
         port: 9000,
         path: '/videocallapp'
     })
