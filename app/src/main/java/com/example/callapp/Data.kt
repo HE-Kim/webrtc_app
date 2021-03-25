c8839de42a4acf43d3ba4aaa78f712c70bae0384
HAEUN <kimhe045@gmail.com>
2021-03-25 15:32:19 +0900

    예현 코트 합치기

--- app/src/main/java/com/example/callapp/Data.kt
+++ app/src/main/java/com/example/callapp/Data.kt
@@ -4,6 +4,7 @@ import java.util.HashMap
 
 data class Data(val result: String)//Accepted)
 data class Data1(val result: String, val login: login)
+
 //data class login(val login: HashMap<String, Friend>)
 data class login(
     val uid: Int,
@@ -22,4 +23,16 @@ data class login(
     val loginIp: String,
     val loginDate: String
 )
+data class contacts(
+    val fidname: String,
+    val chnum: Int,
+    val fstbname: String,
+    val regDate: String,
+    val mac: String,
+    val cid: Int
+)
 data class Data4(val result: String, val stb_id: Int)//Accepted)
+//data class Data5(val result: String, val contacts: List<contacts>)
+data class Data5(val result: String, val contacts: List<contacts>)
+//data class Data5(val result: String)
+//data class Accepted(val accepted: HashMap<String, contacts>)
--- app/src/main/java/com/example/callapp/PhoneBookFragment2.kt
+++ app/src/main/java/com/example/callapp/PhoneBookFragment2.kt
@@ -21,8 +21,39 @@ import com.google.firebase.ktx.Firebase
 //import kotlinx.android.synthetic.main.activity_call.*
 //import kotlinx.android.synthetic.main.activity_call.incomingCallTxt
 import kotlinx.android.synthetic.main.fragment_phone_book.*
+import okhttp3.OkHttpClient
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import retrofit2.http.*
+import java.io.InputStream
+import java.security.KeyStore
+import java.security.cert.Certificate
+import java.security.cert.CertificateException
+import java.security.cert.CertificateFactory
+import java.security.cert.X509Certificate
+import java.util.*
+import javax.net.ssl.*
 
-
+interface CometChatFriendsService_pb2 {
+    @Headers(
+        "accept: application/json",
+        "content-type: application/json"
+    )
+    //@POST("/{value}?id={value_id}&passwd=user1234!&role=50&name=김하은100&contact1=010&contact2=3333&contact3=4444")
+    // @POST("/{value}?id={value_id}&passwd={userpw}&role=50&name={name}&contact1={contact1}&contact2={contact2}&contact3={contact3}")
+    @POST("/api/getContacts?")
+    fun addFriend(
+        @Header("apikey") apiKey: String,
+        @Header("appid") appID: String,
+      //  @Body params: HashMap<String, List<String>>,
+        //@Path("value") value: String,
+        @Query("id") id: String
+    )
+            : Call<Data5>
+}
 @Suppress("UNCHECKED_CAST")
 class PhoneBookFragment2 : Fragment() {
 
@@ -32,6 +63,7 @@ class PhoneBookFragment2 : Fragment() {
     var firebaseRef = Firebase.database.getReference("users")
     var incoming = "false"
     var res_check1 = "false"
+    var res_contacts = ""
 
     var variable = false
 
@@ -47,6 +79,7 @@ class PhoneBookFragment2 : Fragment() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         Log.d(TAG, "phoneBookFragment2-onCreate() called")
+
 
     }
 
@@ -72,7 +105,7 @@ class PhoneBookFragment2 : Fragment() {
         val editSearch = view.findViewById<View>(R.id.userIdEdit) as EditText
 
 
-
+        send()
 
         val adapter = ArrayAdapter<String>(
             view.context,
@@ -149,6 +182,99 @@ class PhoneBookFragment2 : Fragment() {
 
 
     }
+    private fun send() {
+
+
+
+        //   (SSL_Activity.mContext as SSL_Activity).ssl_raw()
+        val cf = CertificateFactory.getInstance("X.509")
+        val caInput: InputStream = resources.openRawResource(R.raw.server)
+        var ca: Certificate? = null
+        try {
+            ca = cf.generateCertificate(caInput)
+            println("ca=" + (ca as X509Certificate?)!!.subjectDN)
+        } catch (e: CertificateException) {
+            e.printStackTrace()
+        } finally {
+            caInput.close()
+        }
+        val keyStoreType = KeyStore.getDefaultType()
+        var keyStore = KeyStore.getInstance(keyStoreType)
+        keyStore.load(null, null)
+        if (ca == null) {
+
+        }
+        keyStore.setCertificateEntry("ca", ca)
+
+
+
+        val trustManagerFactory =
+            TrustManagerFactory.getInstance(TrustManagerFactory.getDefaultAlgorithm())
+        trustManagerFactory.init(keyStore)
+
+
+        val trustManagers: Array<TrustManager> = trustManagerFactory.trustManagers
+        check(!(trustManagers.size != 1 || trustManagers[0] !is X509TrustManager)) {
+            "Unexpected default trust managers:" + Arrays.toString(
+                trustManagers
+            )
+
+        }
+        val hostnameVerifier = HostnameVerifier { _, session ->
+            HttpsURLConnection.getDefaultHostnameVerifier().run {
+                verify("https://13.125.233.161:6443", session)
+            }
+        }
+        val trustManager: X509TrustManager = trustManagers[0] as X509TrustManager
+        val sslContext = SSLContext.getInstance("SSL")
+        sslContext.init(null, arrayOf<TrustManager>(trustManager), null)
+        val sslSocketFactory = sslContext.socketFactory
+        val client1: OkHttpClient.Builder = OkHttpClient.Builder()
+            .sslSocketFactory(sslSocketFactory, trustManager)
+
+        client1.hostnameVerifier(HostnameVerifier { hostname, session -> true })
+
+
+        val retrofit = Retrofit.Builder()
+            .baseUrl("https://13.125.233.161:6443")
+            .client(client1.build())
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+
+
+        //retrofit 객체를 통해 인터페이스 생성
+        val service = retrofit.create(CometChatFriendsService_pb2::class.java)
+
+        val body = HashMap<String, List<String>>()
+
+
+
+        //         "/userReg?id=0test&passwd=user1234!&role=50&name=김하은100&contact1=010&contact2=3333&contact3=4444"
+        //val value="/userReg?id=$userID&passwd=$userpw&role=50&name=$username&contact1=$PhoneNum1&contact2=$PhoneNum2&contact3=$PhoneNum3"
+        val value="userReg"//"userReg?id=$userID&passwd=$userpw&role=50&name=$username&contact1=$PhoneNum1&contact2=$PhoneNum2&contact3=$PhoneNum3"
+        val id=username
+
+        val apiKey = "12"
+        val appID = "123"
+        service.addFriend(
+            apiKey, appID,
+            id
+        )?.enqueue(object : Callback<Data5> {
+            override fun onFailure(call: Call<Data5>, t: Throwable) {
+                Log.d(
+                    "CometChatAPI::", "Failed API call with call: " + call +
+                            " + exception: " + t
+                )
+            }
+
+            override fun onResponse(call: Call<Data5>, response: Response<Data5>) {
+                Log.d("Response5:: ", response.body().toString())
+               // res_contacts= response.body()?.contacts?.fstbname.toString()
+                //Log.d("Response5:: ", res_contacts)
+            }
+        })
+    }
+
 
     private fun initList(listview: ListView, adapter: ArrayAdapter<String>) {
         arguments?.let {
